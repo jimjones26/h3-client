70652f9c6562035c72d83644ef5381e0ee245622
jjones <jimmy.jones@tutanota.com>
2021-01-07 11:25:41 -0700

    remove comments

--- src/routes/verify-email/[token].js
+++ src/routes/verify-email/[token].js
@@ -7,14 +7,11 @@ import {
 export const get = async (req, res) => {
 	// get token from page params
 	const token = req.params.token;
-	console.log('boom', token);
 
 	// call /session API endpoint and pass the token
 	const tokens = await post('http://localhost:5000/api/v1/users/session', {
 		token
 	});
-	console.log('bam ', tokens.accessToken);
-	console.log('bam ', tokens.refreshToken);
 	if (tokens.status === 'success') {
 		res.setHeader('Set-Cookie', [
 			createAccessCookie(tokens.accessToken),
