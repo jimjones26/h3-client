8d2a4ad3a9970b17774f30a8966ad030a46d62f1
jjones <jimmy.jones@tutanota.com>
2021-01-14 21:51:36 -0700

    add notes and ui structure to complete profile page

--- src/routes/complete-profile.svelte
+++ src/routes/complete-profile.svelte
@@ -1,1 +1,38 @@
-complete profile
+<script>
+	import {onMount} from 'svelte';
+	import layoutStore from '../stores/layout-store';
+
+	onMount(() => {
+		layoutStore.setNavHeader(true, 'Complete My Profile', '/dashboard');
+	});
+	/* 
+What am I trying to acomplish on this screen?
+I need to capture two default data points from a new user.
+- practitioner gender preference
+- appointment type preference
+
+Practitioner Gender Preference
+- none
+- male
+- female
+
+Appointment Preference
+- none
+- in person
+- virtual
+
+*/
+</script>
+
+<div class="complete-profile-container">complete my profile</div>
+
+<style>
+	.complete-profile-container {
+		display: grid;
+		grid-template-rows: auto auto 1fr auto;
+		margin-right: 50px;
+		margin-left: 50px;
+		margin-bottom: 32px;
+		margin-top: 40px;
+	}
+</style>
