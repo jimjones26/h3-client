015ad007a44a2f7b8468b46b3f6f337420b54bcd
jjones <jimmy.jones@tutanota.com>
2021-01-22 11:42:34 -0700

    fix scopes not working

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,43 +1,11 @@
 import {Router} from '@beyonk/sapper-rbac';
-import scopes from '../utils/scopes';
-
-/* 
-SCOPES:
-- admin (for the head of an org)
-- client
-- practitioner
-*/
+import {adminScope, clientScope, practitionerScope} from '../utils/scopes';
 
 const routes = new Router()
-	.restrict('/client/dashboard.*', [
-		scopes.adminScope,
-		scopes.clientScope,
-		scopes.practitionerScope
-	])
-	.restrict('/admin.*', [scopes.adminScope])
-	.unrestrict('/')
-	.unrestrict('/login.*')
-	.unrestrict('/check-auth.*')
-	.unrestrict('/verify-email')
-	.unrestrict('/logout.*')
-	.restrict('/.*', [
-		scopes.adminScope,
-		scopes.clientScope,
-		scopes.practitionerScope
-	])
-	// add this after the guarded sub-urls
+	.restrict('/admin.*', [adminScope])
+	.restrict('/clients/.*', [clientScope])
+	.restrict('/practitioners/.*', [practitionerScope])
+	.unrestrict('/.*')
 	.build();
 
 export default routes;
-
-/* 
-LOGIN IS THE ONLY OPEN ROUTE
-if a route is restricted, a user must have one of the included scopes to access it
-if they do not have that scope, they will be redirected, in this case to the check auth server route
-if they have a valid access token, it means they are trying to hit a route they are not authorized for
-    - redirect back to referring route
-if they dont have access token, but do have a refresh token, they need to get new tokens
-    - redirect back to referring route
-if they have neither access or refresh token, they need to be redirected to login
-    - redirect to login
-*/
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,8 +1,27 @@
 <script>
-	import {goto} from '@sapper/app';
 	import layoutStore from '../stores/layout-store';
 	import NavHeader from '../components/NavHeader.svelte';
 	import Loading from '../components/Loading.svelte';
+
+	import routes from '../config/routes.js';
+	import {guard} from '@beyonk/sapper-rbac';
+	import {tick} from 'svelte';
+	import {stores, goto} from '@sapper/app';
+
+	const {page, session} = stores();
+
+	const options = {
+		routes,
+		deny: () => goto('/')
+		// we don't specify grant here, since we don't need to do anything.
+	};
+
+	// Listen to the page store.
+	page.subscribe(async v => {
+		await tick(); // let the previous routing finish first.
+		guard(v.path, $session.profile, options);
+	});
+
 	export let segment;
 
 	async function navigateBack(event) {
