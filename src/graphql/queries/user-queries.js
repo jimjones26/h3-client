8265698d2207f0c3432476808bbfdc4da86e3774
jjones <jimmy.jones@tutanota.com>
2021-01-25 10:14:45 -0700

    save practitioner profile info to db

--- src/graphql/queries/user-queries.js
+++ src/graphql/queries/user-queries.js
@@ -9,7 +9,13 @@ mutation CompleteUserProfile($id: Int!, $genderPreference: String!, $appointment
 `;
 
 export const COMPLETE_PRACTITIONER_PROFILE = `
-
+mutation UpdatePractitionerProfile($id: Int!, $firstName: String!, $lastName: String!, $phoneNumber: String!) {
+  update_h3_users(where: {id: {_eq: $id}}, _set: {first_name: $firstName, last_name: $lastName, phone_number: $phoneNumber, first_visit: false}) {
+    returning {
+      id
+    }
+  }
+}
 `;
 
 export const COMPLETE_EXTENDED_PRACTITIONER_PROFILE = `
