3c0990c95b51d8b63f827af3a3b4c7d31f64b2d4
jjones <jimmy.jones@tutanota.com>
2021-01-29 21:19:47 -0700

    initial styling for practitioner list item

--- src/components/PractitionerListItem.svelte
+++ src/components/PractitionerListItem.svelte
@@ -2,7 +2,43 @@
 	export let practitioner;
 </script>
 
-{practitioner.user.first_name}
-{practitioner.user.last_name}
-{practitioner.session_fee}
-{practitioner.session_time}
+<a href="">
+	<div class="nav-header">
+		{practitioner.user.first_name}
+		{practitioner.user.last_name}<svg class="button-icon">
+			<use xlink:href="../../images/icons.svg#arrow_forward" />
+		</svg>
+	</div>
+	<div class="session-fee">
+		${practitioner.session_fee}/{practitioner.session_time}
+		minutes
+	</div>
+	<div class="modalities">
+		<strong>HH Modalities:</strong>
+		Family, Marriage, Single, Trauma, Heart Healing, Another Modality
+	</div>
+</a>
+
+<style>
+	a {
+		display: grid;
+		grid-template-rows: auto auto;
+		text-decoration: none;
+		color: var(--main-font-color);
+		margin-bottom: 33px;
+	}
+	.nav-header {
+		font-size: 24px;
+	}
+	.modalities {
+		font-size: 16px;
+		margin-top: 5px;
+	}
+	.button-icon {
+		vertical-align: middle;
+		margin-bottom: 2px;
+		margin-left: 6px;
+		width: 24px;
+		height: 24px;
+	}
+</style>
