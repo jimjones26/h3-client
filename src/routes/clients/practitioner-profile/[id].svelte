2594ecc1859a09403420eec4338cabca64cfbff3
jjones <jimmy.jones@tutanota.com>
2021-02-17 13:13:55 -0700

    iniial layout for practitioner detail page

--- src/routes/clients/practitioner-profile/[id].svelte
+++ src/routes/clients/practitioner-profile/[id].svelte
@@ -1,31 +1,62 @@
 <script>
-	import {onMount} from 'svelte';
+	import {onMount, onDestroy} from 'svelte';
 	import {stores} from '@sapper/app';
 	import userStore from '../../../stores/user-store';
 	import layoutStore from '../../../stores/layout-store';
 
-	onMount(() => {
-		layoutStore.setNavHeader(
+	onMount(async () => {
+		await userStore
+			.getPractitionerProfileById($page.params.id)
+			.then(layoutStore.updateLoadingState(false, null));
+
+		await layoutStore.setNavHeader(
 			true,
-			'Find a Practitioner',
+			`${$userStore.selectedPractitioner.user.first_name} ${$userStore.selectedPractitioner.user.last_name}`,
 			'/clients/find-practitioner'
 		);
+
 		layoutStore.updateTermsVisibility(false);
 	});
 
-	const {session} = stores();
 	const {page} = stores();
 
-	if (process.browser) {
-		userStore
-			.getPractitionerProfileById($page.params.id)
-			.then(layoutStore.updateLoadingState(false, null));
-	}
-	//
+	onDestroy(() => {
+		userStore.clearSelectedPractitioner();
+	});
+
+	let practitioner;
+	$: practitioner = $userStore.selectedPractitioner;
 </script>
 
 <svelte:head>
 	<title>Heart Healing Hub - Practitioner Profile</title>
 </svelte:head>
 
-{JSON.stringify($userStore.selectedPractitioner)}
+<div class="detail-container">
+	{#if practitioner}
+		<div>
+			<p>{practitioner.modalities}</p>
+			<p>{practitioner.main_focus}</p>
+			<p>{practitioner.affiliation}</p>
+			<p>{practitioner.biography}</p>
+		</div>
+		<div>buttons</div>
+	{/if}
+</div>
+
+<!-- {JSON.stringify($userStore.selectedPractitioner)} -->
+<style>
+	.detail-container {
+		display: grid;
+		grid-template-rows: 1fr auto;
+		margin-right: 50px;
+		margin-left: 50px;
+		margin-bottom: 32px;
+		margin-top: 40px;
+	}
+
+	p {
+		padding: 0;
+		margin: 0;
+	}
+</style>
